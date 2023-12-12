f5e095ca7150823f3428052fc546b331edaeee31
Mudasir Najar <mudasir1bashir@gmail.com>
2023-12-12 09:19:19 +0530

    Hospital Management System Added

---
+++ src/main/java/com/example/hospital/management/PatientController.java
@@ -0,0 +1,67 @@
+package com.example.hospital.management;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class PatientController {
+
+    HashMap<Integer,Patient> patientDb = new HashMap<>();
+
+    @PostMapping("/addPatientViaParameter")
+    public String addPatient(@RequestParam("patientId") Integer patientId,
+                             @RequestParam("name") String name,
+                             @RequestParam("age") Integer age,
+                             @RequestParam("disease") String disease){
+        Patient patient = new Patient(patientId,name,disease,age);
+        patientDb.put(patientId,patient);
+        return "Patient Added Sucessfully";
+    }
+
+    @PostMapping("/addPatientViaRequestBody")
+    public String addPatient(@RequestBody Patient patient){
+        int key = patient.getPatientId();
+        patientDb.put(key,patient);
+        return "Patient Added Succesfully Via RequestBody";
+    }
+
+    @GetMapping("/getPatientInfo")
+    public Patient getPatient(@RequestParam("patientId") Integer patientId) {
+        Patient patient = patientDb.get(patientId);
+        return patient;
+    }
+
+    @GetMapping("/getPatientByName")
+    public Patient getPatientByName(@RequestParam("name") String name) {
+        for (Patient p : patientDb.values()) {
+            if (p.getName().equals(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/getAllPatients")
+    public List<Patient> getAllPatients() {
+        List<Patient> patients = new ArrayList<>();
+        for (Patient p : patientDb.values()) {
+            patients.add(p);
+        }
+        return patients;
+    }
+
+
+    @GetMapping("/getPatientsListGreaterThanAge")
+    public List<Patient> getPatientsGreaterThanAge(@RequestParam("age") Integer age) {
+        List<Patient> patients = new ArrayList<>();
+        for (Patient p : patientDb.values()) {
+            if (p.getAge() > age) {
+                patients.add(p);
+            }
+        }
+        return patients;
+    }
+}
